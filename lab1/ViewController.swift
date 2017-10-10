bd22d3657e04447ef5f3ff66ae25613e43a67fa4
Użytkownik Gość <Guest@imac-04.local>
2017-10-10 09:27:37 +0200

    Drugi

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var url = URL(string : "https://isebi.net/albums.php")
+        
+        var urlSession = URLSession.shared
+        let taskData = urlSession.dataTask(with: url!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
